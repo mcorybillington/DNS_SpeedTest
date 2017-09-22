5df4ffa1fa94c1757a60adc8bf5d56585e72dbd9
mcorybillington <mcorybillington@gmail.com>
2017-09-22 00:23:31 -0400

    Update dnsspeedtest.sh

--- dnsspeedtest.sh
+++ dnsspeedtest.sh
@@ -2,7 +2,7 @@
 red=`tput setaf 196`
 green=`tput setaf 2`
 reset=`tput sgr0`
-IFS=$'\n'       # make newlines the only separator
+IFS=$'\n'
 for LINE in $(cat ./dnstest.txt)
 do
     echo -ne "$LINE " &&  echo "$LINE" | sed -e 's#.*: \(\)#\1#' | xargs ping -W 1 -c 1 | grep -E "time=|0 received" | echo -e "${green}$(sed 's/^.*time=/time=/')${reset}" | echo -e "${red}$(sed 's/^.*time /time /')${reset}"

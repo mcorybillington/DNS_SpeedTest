085e2ddf540a9c5fd6b21f444cc15ff3a6feb1b1
Cory Billington <mcorybillington@gmail.com>
2017-09-22 01:21:46 -0400

    Changed failed response to TIMEOUT

--- dnsspeedtest.sh
+++ dnsspeedtest.sh
@@ -5,5 +5,5 @@ reset=`tput sgr0`
 IFS=$'\n'       # make newlines the only separator
 for LINE in $(cat ./dnstest.txt)
 do
-    echo -ne "$LINE " &&  echo "$LINE" | sed -e 's#.*: \(\)#\1#' | xargs ping -W 1 -c 1 | grep -E "time=|0 received" | echo -e "${green}$(sed 's/^.*time=/time=/')${reset}" | echo -e "${red}$(sed 's/^.*time /time /')${reset}"
+    echo -ne "$LINE " &&  echo "$LINE" | sed -e 's#.*: \(\)#\1#' | xargs ping -W 1 -c 1 | grep -E "time=|0 received" | echo -e "${green}$(sed 's/^.*time=/time=/')${reset}" | grep "time=" || echo -e "${red}TIMEOUT${reset}"
 done
